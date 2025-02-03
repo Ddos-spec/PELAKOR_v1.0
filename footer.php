941cddc1dc0d47898200cd3e245c71555f6060db
Ddos-spec <setgraph69@gmail.com>
2025-02-04 02:27:58 +0700

    perbaikan tampilan

--- footer.php
+++ footer.php
@@ -1,41 +1,32 @@
-<br><br>
-<footer class="page-footer blue darken-2">
-    <div class="container">
+<footer class="bg-[#005f99] text-white">
+    <div class="container mx-auto py-6">
         <div class="row">
             <div class="col l6 s12">
-            <h5 class="white-text">Company Bio</h5>
-            <p class="grey-text text-lighten-4">
-            Project ini digunakan sebagai Final Project ukom untuk membuat aplikasi laundry dengan penyempurnaan fitur fitur dan keamanan yang cukup mumpuni 
-            </p>
-
-
+                <h5 class="text-lg">Company Bio</h5>
+                <p class="text-gray-200">
+                    Project ini digunakan sebagai Final Project ukom untuk membuat aplikasi laundry dengan penyempurnaan fitur fitur dan keamanan yang cukup mumpuni 
+                </p>
             </div>
             <div class="col l3 s12">
-            <h5 class="white-text">Laundryku</h5>
-            <ul>
-                <li><a class="white-text" href="index.php">Home</a></li>
-                <li><a class="white-text" href="term.php">Term and Condition</a></li>
-                <li><a class="white-text" href="registrasi.php">Registrasi</a></li>
-                <li><a class="white-text" href="login.php">Login</a></li>
-            </ul>
+                <h5 class="text-lg">Laundryku</h5>
+                <ul>
+                    <li><a class="text-white hover:text-[#ffcc33]" href="index.php">Home</a></li>
+                    <li><a class="text-white hover:text-[#ffcc33]" href="term.php">Term and Condition</a></li>
+                    <li><a class="text-white hover:text-[#ffcc33]" href="registrasi.php">Registrasi</a></li>
+                    <li><a class="text-white hover:text-[#ffcc33]" href="login.php">Login</a></li>
+                </ul>
             </div>
             <div class="col l3 s12">
-            <h5 class="white-text">Developer</h5>
-            <ul>
-                <li><a class="white-text" href="team.php">Fadjar Aryo Seto</a></li>
-            </ul>
+                <h5 class="text-lg">Developer</h5>
+                <ul>
+                    <li><a class="text-white hover:text-[#ffcc33]" href="team.php">Fadjar Aryo Seto</a></li>
+                </ul>
             </div>
         </div>
     </div>
-    <div class="footer-copyright">
+    <div class="footer-copyright bg-[#005f99]">
         <div class="container">
-            Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+            Made by <a class="text-[#ffcc33]" href="http://materializecss.com">Materialize</a>
         </div>
     </div>
 </footer>
-
-<!--  Scripts-->
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<!-- end script -->
--- header.php
+++ header.php
@@ -1,121 +1,45 @@
-<nav class="blue darken2">
-    <div class="container">
-        <div class="nav-wrapper">
-            <a id="logo-container" href="index.php" class="brand-logo"><i class="material-icons left large">home</i>LaundryKu</a>
-            <ul class="right hide-on-med-and-down">
+<nav class="bg-[#005f99]">
+    <div class="container mx-auto">
+        <div class="flex justify-between items-center py-4">
+            <a id="logo-container" href="index.php" class="text-white text-lg font-bold"><i class="material-icons left">home</i>LaundryKu</a>
+            <ul class="flex space-x-4">
                 <li> 
                 <?php
                     global $connect;
 
                     if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"])){
-                        // mengambil email dari session
                         $idPelanggan = $_SESSION["pelanggan"];
-                        // cari data di db sesuai $email
                         $data = mysqli_query($connect, "SELECT * FROM pelanggan WHERE id_pelanggan = '$idPelanggan'");
-                        // memasukkan ke array asosiatif
                         $data = mysqli_fetch_assoc($data);
-                        // mengambil data nama dari array
                         $nama = $data["nama"];
 
-                        echo "
-                            <a href='pelanggan.php'><b>$nama</b> (Pelanggan)</a>
-                        ";
-                    }else if ( isset($_SESSION["login-agen"]) && isset($_SESSION["agen"])){
-                        // mengambil email dari session
+                        echo "<a class='text-white' href='pelanggan.php'><b>$nama</b> (Pelanggan)</a>";
+                    } else if ( isset($_SESSION["login-agen"]) && isset($_SESSION["agen"])){
                         $id_agen = $_SESSION["agen"];
-                        // cari data di db sesuai $id_agen
                         $data = mysqli_query($connect, "SELECT * FROM agen WHERE id_agen = '$id_agen'");
-                        // memasukkan ke array asosiatif
                         $data = mysqli_fetch_assoc($data);
-                        // mengambil data nama dari array
                         $nama = $data["nama_laundry"];
 
-                        echo "
-                            <a href='agen.php'><b>$nama</b> (Agen)</a>
-                        ";
-                    }else if ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"])){
-                        echo "
-                            <a href='admin.php'><span><b>Admin</b> (Admin)</a>
-                        ";
-                    }else {
-                        echo "
-                            <a href='registrasi.php'><b>Registrasi</b></a>
-                        ";
+                        echo "<a class='text-white' href='agen.php'><b>$nama</b> (Agen)</a>";
+                    } else if ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"])){
+                        echo "<a class='text-white' href='admin.php'><b>Admin</b> (Admin)</a>";
+                    } else {
+                        echo "<a class='text-white' href='registrasi.php'><b>Registrasi</b></a>";
                     }
                 ?>
                 </li>
                 <li>
                 <?php
                     if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) || isset($_SESSION["login-agen"]) && isset($_SESSION["agen"]) || isset($_SESSION["login-admin"]) && isset($_SESSION["admin"]) ){
-                        echo "
-                            <a href='logout.php'><b>Logout</b></a>
-                        ";
-                    }else {
-                        echo "
-                            <a href='login.php'><b>Login</b></a>
-                        ";
+                        echo "<a class='text-white' href='logout.php'><b>Logout</b></a>";
+                    } else {
+                        echo "<a class='text-white' href='login.php'><b>Login</b></a>";
                     }
                 ?>                                      
                 </li>
             </ul>
 
-            <ul id="nav-mobile" class="sidenav">
-                <li>
-                    <?php
-                        if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) || isset($_SESSION["login-agen"]) && isset($_SESSION["agen"]) || isset($_SESSION["login-admin"]) && isset($_SESSION["admin"]) ){
-                            echo "
-                                <a href='logout.php'><b>Logout</b></a>
-                            ";
-                        }else {
-                            echo "
-                                <a href='login.php'><b>Login</b></a>
-                            ";
-                        }
-                    ?>                                      
-                </li>
-                <li> 
-                    <?php
-                        global $connect;
-
-                        if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"])){
-                            // mengambil email dari session
-                            $idPelanggan = $_SESSION["pelanggan"];
-                            // cari data di db sesuai $email
-                            $data = mysqli_query($connect, "SELECT * FROM pelanggan WHERE id_pelanggan = '$idPelanggan'");
-                            // memasukkan ke array asosiatif
-                            $data = mysqli_fetch_assoc($data);
-                            // mengambil data nama dari array
-                            $nama = $data["nama"];
-
-                            echo "
-                                <a href='pelanggan.php'><b>$nama</b> (Pelanggan)</a>
-                            ";
-                        }else if ( isset($_SESSION["login-agen"]) && isset($_SESSION["agen"])){
-                            // mengambil email dari session
-                            $id_agen = $_SESSION["agen"];
-                            // cari data di db sesuai $id_agen
-                            $data = mysqli_query($connect, "SELECT * FROM agen WHERE id_agen = '$id_agen'");
-                            // memasukkan ke array asosiatif
-                            $data = mysqli_fetch_assoc($data);
-                            // mengambil data nama dari array
-                            $nama = $data["nama_laundry"];
-
-                            echo "
-                                <a href='agen.php'><b>$nama</b> (Agen)</a>
-                            ";
-                        }else if ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"])){
-                            echo "
-                                <a href='admin.php'><b>Admin</b> (Admin)</a>
-                            ";
-                        }else {
-                            echo "
-                                <a href='registrasi.php'><b>Registrasi</b></a>
-                            ";
-                        }
-                    ?>
-                </li>
-            </ul>
-            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons text-white">menu</i></a>
         </div>
     </div>
 </nav>
