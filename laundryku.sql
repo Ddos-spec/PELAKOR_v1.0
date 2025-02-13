511923f142d697c1ee77eeb0283820da2be5f02a
Ddos-spec <setgraph69@gmail.com>
2025-02-13 23:24:51 +0700

    Refactor dependencies and remove unused files

--- laundryku.sql
+++ laundryku.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Feb 2025 pada 14.23
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.0.30
+-- Generation Time: May 06, 2020 at 05:49 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,26 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
   `username` varchar(30) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+(1, 'admin2', 'admin');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `agen`
+-- Table structure for table `agen`
 --
 
 CREATE TABLE `agen` (
@@ -56,26 +56,25 @@ CREATE TABLE `agen` (
   `alamat` varchar(100) DEFAULT NULL,
   `plat_driver` varchar(12) DEFAULT NULL,
   `foto` text NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `harga_kiloan` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `agen`
+-- Dumping data for table `agen`
 --
 
-INSERT INTO `agen` (`id_agen`, `nama_laundry`, `nama_pemilik`, `telp`, `email`, `kota`, `alamat`, `plat_driver`, `foto`, `password`, `harga_kiloan`) VALUES
-(17, 'pelakor kilat', 'guntur', '083457682536', 'kilat@gmail.com', 'sdfffd', 'esafcfecef', '21312', '679f3ff7cbdcf.jpg', '$2y$10$LVamXEoKr3.9QG1am8XF2uQXS9pIYZoLXt7xF2To.JSvWawTTO/Nm', 0),
-(18, 'Pelakor Fresh', 'levi', '083457682536', 'fresh@gmail.com', 'hbvabdabh', 'gyjdchgd', '1231', '679f40108cb18.jpg', '$2y$10$xbte90xvoI516x7UhdAKnO9KDdJos6we5.XrGB3aIB46J990xz11S', 0),
-(19, 'Pelakor bubble', 'king', '082346273412', 'bubble@gmail.com', 'dgvssdvd', 'gvdsbvsvs', '2313', '679f4029e0c7a.jpg', '$2y$10$lCuRaDBW6KxvbkKnBtkc.up6Jku8kyTxQBFC78Ca3qWWTzdwBKCwW', 0),
-(20, 'Pelakor Clean', 'coki', '096216372151', 'clean@gmail.com', 'scaca', 'fcsacfadcsa', '123213', '679f404468194.jpg', '$2y$10$mCk62rD4.rU9Vyamb5CeFOJ5CgEALNza4QU02aNRfPob3cRniroBS', 0),
-(21, 'Pelakor cemerlang ', 'cemer', '1243141431', 'cemerlang@gmail.com', 'fafcafqwaf', 'cafafadfwa', '1231', '679f9d4f98daf.jpg', '$2y$10$Z1/.1kGb8xmqSTQ92kG7AuCMoY8hnC0xdx4y85lXTaPk6f8Tp3NrK', 0),
-(22, 'Pelakor bersinar', 'bersinar', '0865276316', 'bersinar@gmail.com', 'afadwwad', 'dawdwad', 'adafvaf', '679f9d9c64589.jpg', '$2y$10$pzATqcMWJaxHFUhzsS1tAeiQfETALGbdiA8bNXApN5wh.g/jI5TM6', 0);
+INSERT INTO `agen` (`id_agen`, `nama_laundry`, `nama_pemilik`, `telp`, `email`, `kota`, `alamat`, `plat_driver`, `foto`, `password`) VALUES
+(1, 'Nadya LaundryKu', 'Nadya Eka', '083123456789', 'agen1@gmail.com', 'Denpasar', 'Jl. Diponegoro No 55', 'DK 1234 AA', '5eab524e20d98.jpg', '$2y$10$tQ4th/nx/LLxYB7iHpbg4.FX1wdffLb5yplJIJsTdU6XlUCNPgEC6'),
+(4, 'Laundry 2', 'Firdaus', '3875120', 'agen2@gmail.com', 'Surabaya', 'Jl. Surabaya No 12', 'DK 0000 AA', 'default.png', '$2y$10$mmXlXG97cauDgYemQwPjKuScxSjrnSrTZMH04bb1dosa7luvj1yUW'),
+(5, 'Laundry WINA GANS', 'Wina Arth', '57109', 'agen3@gmail.com', 'Badung', 'Kuta No 22', 'DK 1234 AA', 'default.png', '$2y$10$tKrLGx8FMw8sCwuxIdUWgevKb0ikEozi8xseBV9CvBzUnUhOkHd1S'),
+(7, 'Hairul Laundry', 'Hairul Lana', '08321456378', 'agen5@gmail.com', 'Karangasem', 'Jl. Mawar No 78', 'DK 5432 AB', 'default.png', '$2y$10$ldHD7JtlC26H.EuNf.kMPO9aamXxsO3yRWagW/gKzUrWjcWezq/eO'),
+(9, 'Satan Laundry', 'Satan', '098527815618', 'agen6@gmail.com', 'Denpasar', 'Jl. Hehe No 77', 'DK 6666 DD', 'default.png', '$2y$10$IS1G8nhOpgY2EeVXppcz1u5sX.enw50eNYkRy9lli2wpnhlCu7PZG'),
+(10, 'IBM Laundry', 'IBM Shava', '083123456789', 'agen4@gmail.com', 'Bangli', 'Jl. Sumatra No 33', 'DK 5555 SM', 'default.png', '$2y$10$OB2C9R4kUHrhGaMi3eF6z.DrDnQzapZhPZTucYh4I.ckes73MoFxy');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `cucian`
+-- Table structure for table `cucian`
 --
 
 CREATE TABLE `cucian` (
@@ -90,12 +89,27 @@ CREATE TABLE `cucian` (
   `alamat` varchar(100) NOT NULL,
   `catatan` text NOT NULL,
   `status_cucian` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cucian`
+--
+
+INSERT INTO `cucian` (`id_cucian`, `id_agen`, `id_pelanggan`, `tgl_mulai`, `tgl_selesai`, `jenis`, `total_item`, `berat`, `alamat`, `catatan`, `status_cucian`) VALUES
+(1, 1, 11, '2020-04-25', '0000-00-00', 'setrika', 2, 1, 'Jl. Aceg No 44, Aceh', 'tak ada', 'Selesai'),
+(2, 5, 8, '2020-04-25', '0000-00-00', 'komplit', 6, 4, 'Jl. Melati No 99, Denpasar', 'yang bersih yaaaa', 'Selesai'),
+(3, 1, 11, '2020-04-26', '0000-00-00', 'cuci', 1, 5, 'Jl. Aceg No 44, Aceh', 'cepet ya', 'Selesai'),
+(4, 4, 11, '2020-04-27', '0000-00-00', 'cuci', 1, 5, 'Jl. Aceg No 44, Aceh', 'cepet', 'Selesai'),
+(5, 5, 11, '2020-04-27', '0000-00-00', 'komplit', 5, 6, 'Jl. Aceg No 44, Aceh', 'yg bersih y', 'Selesai'),
+(6, 7, 9, '2020-04-27', '0000-00-00', 'setrika', 1, NULL, 'Jl. Goa Gong, No 99, Kec Kuta Selatan (Rumah warna hitam), Badung', 'ngebut ya\r\n', 'Penjemputan'),
+(7, 5, 12, '2020-04-29', '0000-00-00', 'setrika', 4, 2, 'Jl. Umum No 77, Singaraja', 'yang sabar', 'Sedang Di Jemur'),
+(8, 5, 12, '2020-05-06', '0000-00-00', 'setrika', 5, 3, 'Jl. Umum No 77, Singaraja', 'Yang Harum ya beb', 'Sedang di Cuci'),
+(9, 5, 13, '2020-05-06', '0000-00-00', 'komplit', 1, 1, 'Jl. Semarang No 99, Semarang', 'tes', 'Selesai');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `harga`
+-- Table structure for table `harga`
 --
 
 CREATE TABLE `harga` (
@@ -103,12 +117,48 @@ CREATE TABLE `harga` (
   `jenis` varchar(30) NOT NULL,
   `id_agen` int(11) NOT NULL,
   `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `harga`
+--
+
+INSERT INTO `harga` (`id_harga`, `jenis`, `id_agen`, `harga`) VALUES
+(1, 'cuci', 3, 2000),
+(2, 'setrika', 3, 1000),
+(3, 'komplit', 3, 2500),
+(4, 'cuci', 1, 5000),
+(5, 'setrika', 1, 3000),
+(6, 'komplit', 1, 7000),
+(7, 'cuci', 4, 300),
+(8, 'setrika', 4, 200),
+(9, 'komplit', 4, 400),
+(10, 'cuci', 5, 4000),
+(11, 'setrika', 5, 3000),
+(12, 'komplit', 5, 5000),
+(13, 'cuci', 6, 7000),
+(14, 'setrika', 6, 3000),
+(15, 'komplit', 6, 8000),
+(16, 'cuci', 7, 3000),
+(17, 'setrika', 7, 2000),
+(18, 'komplit', 7, 4500),
+(19, 'cuci', 8, 6000),
+(20, 'setrika', 8, 3000),
+(21, 'komplit', 8, 7500),
+(22, 'cuci', 9, 4000),
+(23, 'setrika', 9, 2000),
+(24, 'komplit', 9, 5000),
+(25, 'cuci', 10, 5000),
+(26, 'setrika', 10, 3000),
+(27, 'komplit', 10, 6000),
+(28, 'cuci', 10, 5000),
+(29, 'setrika', 10, 3000),
+(30, 'komplit', 10, 6000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -120,24 +170,23 @@ CREATE TABLE `pelanggan` (
   `alamat` varchar(100) DEFAULT NULL,
   `foto` text NOT NULL,
   `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `pelanggan`
+-- Dumping data for table `pelanggan`
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `nama`, `email`, `telp`, `kota`, `alamat`, `foto`, `password`) VALUES
-(16, 'nagumo', 'nagumo@gmail.com', '0852416738425', 'sfff', 'dascef', '679f92aaf10e7.jpg', '$2y$10$.mFleJveky0htDVyUP7w9epQMPyJvdJd1.o6Klm1rLow8yI06xZHO'),
-(17, 'sakamoto', 'sakamoto@gmail.com', '0837152732167', 'sfcdad', 'fasfcasca', '679f9340e709b.jpg', '$2y$10$Am.V5AeggTtKVoeYKIsJDeZgdJdznDJkzDOHPxt/wU6yf26hf5IyK'),
-(18, 'uzuki', 'uzuki@gmail.com', '0812321436', 'adawdwa', 'dwadwad', '679f937d333e1.jpg', '$2y$10$o9d0R4gw3/kbasIlLEp9U.PYGyzurCiCR2VyAOSOFdb3g2bJz8v1i'),
-(19, 'sisiba', 'sisiba@gmail.com', '082143162365', 'afcvas', 'fsaasafqwa', '679f93ac689ec.jpg', '$2y$10$sQ.X7qF/0eC.yJi5pt2U2.HT5ran/KNP5lRy2oHujc5DMXA0ForZW'),
-(20, 'osaragi', 'osaragi@gmail.com', '081231634', 'afcfvrg', 'sefecef', '679f93ff10102.jpg', '$2y$10$5U/fcEGOWRM3HNHDYmKnEu0r9gbu7ltVXzZpbxgW0.wPr5VzvyO4.'),
-(21, 'rion', 'rion@gmail.com', '082143124', 'afcvfsfgew', 'fefescff', '679f97f949134.jpg', '$2y$10$9aCv5ItZuLxzA95qb4QjMehw2Uo6YG.c.5o2jIe.UORc1hgBTGng6');
+(8, 'Pelanggan 1', 'pelanggan1@gmail.com', '0897654321', 'Denpasar', 'Jl. Melati No 99', 'default.png', '$2y$10$Q/LTUi2tH9UawYdI5ynTJe5vq.ga.mIKfTmr7ErtprUQgRkK.pmrG'),
+(9, 'Nadya Eka', 'pelanggan2@gmail.com', '08123456789', 'Badung', 'Jl. Goa Gong, No 99, Kec Kuta Selatan (Rumah warna hitam)', 'default.png', '$2y$10$wvrs6fZ4riwS7j/QoQ1ERunXsVS3a4JBzmaGEMkZEE.2xRGjnVB5G'),
+(11, 'Hairul Lana', 'pelanggan4@gmail.com', '082134567', 'Aceh', 'Jl. Aceg No 44', 'default.png', '$2y$10$XmHjcO/uFSqjtYnwdMAtG.wN/hFJaP2RmX4ObfKXHzYtWzrq88ml6'),
+(12, 'Nadya Okta Via', 'pelanggan5@gmail.com', '089764532132', 'Singaraja', 'Jl. Umum No 77', '5eb222e525b06.jpg', '$2y$10$jxuKyuzIQS3wSYXxcOmde.d26tWIBPf1dpP01IVqDCdKSkkOmEGU.'),
+(13, 'Riski Atma', 'pelanggan3@gmail.com', '09864738429', 'Semarang', 'Jl. Semarang No 99', 'default.png', '$2y$10$rv2iH7OayCjL6.84.9uA8.gaC4lTDzcxG.btFrB6JB4H4mNw5Vxpi');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
 CREATE TABLE `transaksi` (
@@ -149,89 +198,100 @@ CREATE TABLE `transaksi` (
   `tgl_selesai` date DEFAULT NULL,
   `total_bayar` int(11) DEFAULT NULL,
   `rating` int(11) DEFAULT NULL,
-  `komentar` text NOT NULL,
-  `tipe_transaksi` enum('kiloan','satuan') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `komentar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`kode_transaksi`, `id_cucian`, `id_agen`, `id_pelanggan`, `tgl_mulai`, `tgl_selesai`, `total_bayar`, `rating`, `komentar`) VALUES
+(6, 1, 1, 11, '2020-04-25', '2020-04-26', 1000, 6, 'Mantap'),
+(19, 4, 4, 11, '2020-04-27', '2020-04-27', 10000, 10, 'Reccomended'),
+(20, 3, 1, 11, '2020-04-26', '2020-04-29', 10000, 10, 'Sangat cocok, agennya ramah sampe ke ubun ubun'),
+(21, 2, 5, 8, '2020-04-25', '2020-05-06', 10000, 0, ''),
+(22, 5, 5, 11, '2020-04-27', '2020-05-06', 15000, 0, ''),
+(23, 9, 5, 13, '2020-05-06', '2020-05-06', 2500, 10, 'Sangat direkomendasikan');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indeks untuk tabel `agen`
+-- Indexes for table `agen`
 --
 ALTER TABLE `agen`
   ADD PRIMARY KEY (`id_agen`);
 
 --
--- Indeks untuk tabel `cucian`
+-- Indexes for table `cucian`
 --
 ALTER TABLE `cucian`
   ADD PRIMARY KEY (`id_cucian`);
 
 --
--- Indeks untuk tabel `harga`
+-- Indexes for table `harga`
 --
 ALTER TABLE `harga`
   ADD PRIMARY KEY (`id_harga`);
 
 --
--- Indeks untuk tabel `pelanggan`
+-- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indeks untuk tabel `transaksi`
+-- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`kode_transaksi`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
+-- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `agen`
+-- AUTO_INCREMENT for table `agen`
 --
 ALTER TABLE `agen`
-  MODIFY `id_agen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_agen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `cucian`
+-- AUTO_INCREMENT for table `cucian`
 --
 ALTER TABLE `cucian`
-  MODIFY `id_cucian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_cucian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `harga`
+-- AUTO_INCREMENT for table `harga`
 --
 ALTER TABLE `harga`
-  MODIFY `id_harga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `id_harga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT untuk tabel `pelanggan`
+-- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT untuk tabel `transaksi`
+-- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `kode_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `kode_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
