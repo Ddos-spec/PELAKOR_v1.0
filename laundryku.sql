cdc78e422c392911acaafb60e0fbc475e1a4ffbc
Ddos-spec <setgraph69@gmail.com>
2025-02-15 15:41:19 +0700

    y

--- laundryku.sql
+++ laundryku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Feb 2025 pada 05.58
+-- Waktu pembuatan: 15 Feb 2025 pada 09.40
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
@@ -65,11 +65,7 @@ CREATE TABLE `agen` (
 
 INSERT INTO `agen` (`id_agen`, `nama_laundry`, `nama_pemilik`, `telp`, `email`, `kota`, `alamat`, `plat_driver`, `foto`, `password`) VALUES
 (1, 'Nadya LaundryKu', 'Nadya Eka', '083123456789', 'agen1@gmail.com', 'Denpasar', 'Jl. Diponegoro No 55', 'DK 1234 AA', '5eab524e20d98.jpg', '$2y$10$tQ4th/nx/LLxYB7iHpbg4.FX1wdffLb5yplJIJsTdU6XlUCNPgEC6'),
-(4, 'Laundry 2', 'Firdaus', '3875120', 'agen2@gmail.com', 'Surabaya', 'Jl. Surabaya No 12', 'DK 0000 AA', 'default.png', '$2y$10$mmXlXG97cauDgYemQwPjKuScxSjrnSrTZMH04bb1dosa7luvj1yUW'),
-(5, 'Laundry WINA GANS', 'Wina Arth', '57109', 'agen3@gmail.com', 'Badung', 'Kuta No 22', 'DK 1234 AA', 'default.png', '$2y$10$tKrLGx8FMw8sCwuxIdUWgevKb0ikEozi8xseBV9CvBzUnUhOkHd1S'),
-(7, 'Hairul Laundry', 'Hairul Lana', '08321456378', 'agen5@gmail.com', 'Karangasem', 'Jl. Mawar No 78', 'DK 5432 AB', 'default.png', '$2y$10$ldHD7JtlC26H.EuNf.kMPO9aamXxsO3yRWagW/gKzUrWjcWezq/eO'),
-(9, 'Satan Laundry', 'Satan', '098527815618', 'agen6@gmail.com', 'Denpasar', 'Jl. Hehe No 77', 'DK 6666 DD', 'default.png', '$2y$10$IS1G8nhOpgY2EeVXppcz1u5sX.enw50eNYkRy9lli2wpnhlCu7PZG'),
-(10, 'IBM Laundry', 'IBM Shava', '083123456789', 'agen4@gmail.com', 'Bangli', 'Jl. Sumatra No 33', 'DK 5555 SM', 'default.png', '$2y$10$OB2C9R4kUHrhGaMi3eF6z.DrDnQzapZhPZTucYh4I.ckes73MoFxy');
+(12, 'dacafacaf', 'sadacafwa', '2143124214124', 'bakwan@gmail.com', 'ffesfafqwfr', 'adaff', '123131', 'default.png', '$2y$10$H8dYj7pQO11eoX1C1AV03OdusFtwpBQVI/3Rf0uYA7k3tAhnbnD4e');
 
 -- --------------------------------------------------------
 
@@ -84,11 +80,13 @@ CREATE TABLE `cucian` (
   `tgl_mulai` date NOT NULL,
   `tgl_selesai` date NOT NULL,
   `jenis` varchar(15) DEFAULT NULL,
+  `estimasi_item` text DEFAULT NULL,
   `tipe_layanan` enum('kiloan','satuan') NOT NULL DEFAULT 'kiloan',
   `total_item` int(11) DEFAULT NULL,
   `berat` double DEFAULT NULL,
   `alamat` varchar(100) NOT NULL,
   `catatan` text NOT NULL,
+  `catatan_berat` text DEFAULT NULL,
   `status_cucian` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -96,16 +94,17 @@ CREATE TABLE `cucian` (
 -- Dumping data untuk tabel `cucian`
 --
 
-INSERT INTO `cucian` (`id_cucian`, `id_agen`, `id_pelanggan`, `tgl_mulai`, `tgl_selesai`, `jenis`, `tipe_layanan`, `total_item`, `berat`, `alamat`, `catatan`, `status_cucian`) VALUES
-(1, 1, 11, '2020-04-25', '0000-00-00', 'setrika', 'kiloan', 2, 1, 'Jl. Aceg No 44, Aceh', 'tak ada', 'Selesai'),
-(2, 5, 8, '2020-04-25', '0000-00-00', 'komplit', 'kiloan', 6, 4, 'Jl. Melati No 99, Denpasar', 'yang bersih yaaaa', 'Selesai'),
-(3, 1, 11, '2020-04-26', '0000-00-00', 'cuci', 'kiloan', 1, 5, 'Jl. Aceg No 44, Aceh', 'cepet ya', 'Selesai'),
-(4, 4, 11, '2020-04-27', '0000-00-00', 'cuci', 'kiloan', 1, 5, 'Jl. Aceg No 44, Aceh', 'cepet', 'Selesai'),
-(5, 5, 11, '2020-04-27', '0000-00-00', 'komplit', 'kiloan', 5, 6, 'Jl. Aceg No 44, Aceh', 'yg bersih y', 'Selesai'),
-(6, 7, 9, '2020-04-27', '0000-00-00', 'setrika', 'kiloan', 1, NULL, 'Jl. Goa Gong, No 99, Kec Kuta Selatan (Rumah warna hitam), Badung', 'ngebut ya\r\n', 'Penjemputan'),
-(7, 5, 12, '2020-04-29', '0000-00-00', 'setrika', 'kiloan', 4, 2, 'Jl. Umum No 77, Singaraja', 'yang sabar', 'Sedang Di Jemur'),
-(8, 5, 12, '2020-05-06', '0000-00-00', 'setrika', 'kiloan', 5, 3, 'Jl. Umum No 77, Singaraja', 'Yang Harum ya beb', 'Sedang di Cuci'),
-(9, 5, 13, '2020-05-06', '0000-00-00', 'komplit', 'kiloan', 1, 1, 'Jl. Semarang No 99, Semarang', 'tes', 'Selesai');
+INSERT INTO `cucian` (`id_cucian`, `id_agen`, `id_pelanggan`, `tgl_mulai`, `tgl_selesai`, `jenis`, `estimasi_item`, `tipe_layanan`, `total_item`, `berat`, `alamat`, `catatan`, `catatan_berat`, `status_cucian`) VALUES
+(1, 1, 11, '2020-04-25', '0000-00-00', 'setrika', NULL, 'kiloan', 2, 1, 'Jl. Aceg No 44, Aceh', 'tak ada', NULL, 'Selesai'),
+(2, 5, 8, '2020-04-25', '0000-00-00', 'komplit', NULL, 'kiloan', 6, 4, 'Jl. Melati No 99, Denpasar', 'yang bersih yaaaa', NULL, 'Selesai'),
+(3, 1, 11, '2020-04-26', '0000-00-00', 'cuci', NULL, 'kiloan', 1, 5, 'Jl. Aceg No 44, Aceh', 'cepet ya', NULL, 'Selesai'),
+(4, 4, 11, '2020-04-27', '0000-00-00', 'cuci', NULL, 'kiloan', 1, 5, 'Jl. Aceg No 44, Aceh', 'cepet', NULL, 'Selesai'),
+(5, 5, 11, '2020-04-27', '0000-00-00', 'komplit', NULL, 'kiloan', 5, 6, 'Jl. Aceg No 44, Aceh', 'yg bersih y', NULL, 'Selesai'),
+(6, 7, 9, '2020-04-27', '0000-00-00', 'setrika', NULL, 'kiloan', 1, NULL, 'Jl. Goa Gong, No 99, Kec Kuta Selatan (Rumah warna hitam), Badung', 'ngebut ya\r\n', NULL, 'Penjemputan'),
+(7, 5, 12, '2020-04-29', '0000-00-00', 'setrika', NULL, 'kiloan', 4, 2, 'Jl. Umum No 77, Singaraja', 'yang sabar', NULL, 'Sedang Di Jemur'),
+(8, 5, 12, '2020-05-06', '0000-00-00', 'setrika', NULL, 'kiloan', 5, 3, 'Jl. Umum No 77, Singaraja', 'Yang Harum ya beb', NULL, 'Sedang di Cuci'),
+(9, 5, 13, '2020-05-06', '0000-00-00', 'komplit', NULL, 'kiloan', 1, 1, 'Jl. Semarang No 99, Semarang', 'tes', NULL, 'Selesai'),
+(10, 1, 14, '2025-02-15', '0000-00-00', 'komplit', 'ada 100 baju', 'kiloan', NULL, NULL, '', '', NULL, 'Penjemputan');
 
 -- --------------------------------------------------------
 
@@ -197,7 +196,9 @@ INSERT INTO `harga_satuan` (`id_harga_satuan`, `id_agen`, `nama_item`, `jenis`, 
 (6, 1, 'Celana', 'komplit', 15000),
 (7, 1, 'Jaket', 'cuci', 15000),
 (8, 1, 'Jaket', 'setrika', 10000),
-(9, 1, 'Jaket', 'komplit', 20000);
+(9, 1, 'Jaket', 'komplit', 20000),
+(10, 12, 'adawda', '', 1000),
+(11, 12, 'dawda', '', 10000);
 
 -- --------------------------------------------------------
 
@@ -215,18 +216,6 @@ CREATE TABLE `pelanggan` (
   `foto` text NOT NULL,
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `pelanggan`
---
-
-INSERT INTO `pelanggan` (`id_pelanggan`, `nama`, `email`, `telp`, `kota`, `alamat`, `foto`, `password`) VALUES
-(8, 'Pelanggan 1', 'pelanggan1@gmail.com', '0897654321', 'Denpasar', 'Jl. Melati No 99', 'default.png', '$2y$10$Q/LTUi2tH9UawYdI5ynTJe5vq.ga.mIKfTmr7ErtprUQgRkK.pmrG'),
-(9, 'Nadya Eka', 'pelanggan2@gmail.com', '08123456789', 'Badung', 'Jl. Goa Gong, No 99, Kec Kuta Selatan (Rumah warna hitam)', 'default.png', '$2y$10$wvrs6fZ4riwS7j/QoQ1ERunXsVS3a4JBzmaGEMkZEE.2xRGjnVB5G'),
-(11, 'Hairul Lana', 'pelanggan4@gmail.com', '082134567', 'Aceh', 'Jl. Aceg No 44', 'default.png', '$2y$10$XmHjcO/uFSqjtYnwdMAtG.wN/hFJaP2RmX4ObfKXHzYtWzrq88ml6'),
-(12, 'Nadya Okta Via', 'pelanggan5@gmail.com', '089764532132', 'Singaraja', 'Jl. Umum No 77', '5eb222e525b06.jpg', '$2y$10$jxuKyuzIQS3wSYXxcOmde.d26tWIBPf1dpP01IVqDCdKSkkOmEGU.'),
-(13, 'Riski Atma', 'pelanggan3@gmail.com', '09864738429', 'Semarang', 'Jl. Semarang No 99', 'default.png', '$2y$10$rv2iH7OayCjL6.84.9uA8.gaC4lTDzcxG.btFrB6JB4H4mNw5Vxpi'),
-(14, 'don', 'sakamoto@gmail.com', '1431313231231', 'dadadawda', 'adqwdqada', 'default.png', '$2y$10$c2DlNF/TWHeDzb4F8.tytePxdEU954rsIobU4mfd370vnj8XjVCwK');
 
 -- --------------------------------------------------------
 
@@ -327,13 +316,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT untuk tabel `agen`
 --
 ALTER TABLE `agen`
-  MODIFY `id_agen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_agen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `cucian`
 --
 ALTER TABLE `cucian`
-  MODIFY `id_cucian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_cucian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `detail_cucian`
@@ -351,7 +340,7 @@ ALTER TABLE `harga`
 -- AUTO_INCREMENT untuk tabel `harga_satuan`
 --
 ALTER TABLE `harga_satuan`
-  MODIFY `id_harga_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_harga_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `pelanggan`
