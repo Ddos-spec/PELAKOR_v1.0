e9406bcd2f3acba68e9855f42daa0ddab7957ac8
Ddos-spec <setgraph69@gmail.com>
2025-02-17 14:00:16 +0700

    revision

--- materialize/js/script.js
+++ materialize/js/script.js
@@ -1,23 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Initialize sidenav with passive listeners
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-        draggable: true,
-        preventScrolling: true,
-        onOpenStart: function() {
-            document.body.style.overflow = 'hidden';
-        },
-        onCloseEnd: function() {
-            document.body.style.overflow = '';
-        }
-    });
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
 
-    // Initialize slider if used
-    var sliders = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(sliders, {
-        indicators: true,
-        height: 400,
-        duration: 500,
-        interval: 6000
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
 });
